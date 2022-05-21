5fc050a3f686880ba4f94a71d98943b5171bf102
Jubayer Hosen <jubayerhosain9838@gmail.com>
2022-05-22 03:36:02 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World! Server is Running');
+})
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+})
