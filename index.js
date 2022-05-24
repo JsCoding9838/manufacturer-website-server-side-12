32e96f8e0b1a7161aaca0b7809bb22d997b9fd60
Jubayer Hosen <jubayerhosain9838@gmail.com>
2022-05-24 21:04:52 +0800

    send a spacific service product from db

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
+const { get } = require('express/lib/response');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -10,17 +11,29 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
-//user:jubayer5230
-//pas: bpdDGrsOA6HawuOn
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4vvso.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message: 'Unauthorization'});
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+    if(err){
+      res.status(403).send({message: 'Forbidden Access'})
+    }
+    req.decoded = decoded;
+    next();
+  });
+  // console.log('verify JWT');
+}
+
 async function run() {
   try {
     await client.connect();
-    console.log('connect database')
     const toolsCollection = client.db("manufacturer_care").collection("tools");
     const userCollection = client.db("manufacturer_care").collection("users");
     
@@ -32,21 +45,35 @@ async function run() {
       res.send(services);
     });
 
+    // get a specific service tool by id
+    app.get('/products/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id:ObjectId(id)};
+      const result = await toolsCollection.findOne(query);
+      res.send(result);
+      console.log(req);
+    })
+
+
     // user create
     app.put('/user/:email', async(req, res) => {
-      const email = req.params.email;
-
+      
+   
+     
       const user = req.body;
-      console.log(email, user)
-      const filter = {email: email};
-      const options = { upsert: true };
+      if(user ){
+        console.log( user.email, user.displayName)
+        const email = user.email
+        const filter = {email: email};
+        const options = { upsert: true };
 
       const updateDoc = {
-        $set: user,
+        $set:{email: email, displayName: user.displayName},
       };
       const result = await userCollection.updateOne(filter, updateDoc, options);
       const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET);
       res.send({result, token});
+      }
     })
     
   } 
@@ -55,6 +82,10 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+
+
+
 
 
 
